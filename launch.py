5a12af91f1ee415f646ef36ee3d6d9b984b2ce9f
ankitbanga <ankit.it13@gmail.com>
2025-09-25 11:33:50 +0530

    runs on the automation of build

--- launch.py
+++ launch.py
@@ -2,6 +2,6 @@ from flask import Flask
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
-    return "{\"message\":\"Hello World Python, this is tested images and able to connect on browser v1\"}"
+    return "{\"message\":\"Hello World Python, this is tested images and able to connect on browser and test for the automation v2\"}"
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
